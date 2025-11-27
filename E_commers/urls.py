858b070b5d9352808a55494db3e00f11a96501c4
VivekMane57 <vivekmane.9731@gmail.com>
2025-11-27 21:55:34 +0530

    Serve media files and add product images

--- E_commers/urls.py
+++ E_commers/urls.py
@@ -1,28 +1,3 @@
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from . import views
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-    
-#     # Homepage
-#     path('', views.home, name='home'),
-    
-#     # Apps
-#     path('accounts/', include('accounts.urls')),
-#     path('store/', include('store.urls')),
-#     path('cart/', include('carts.urls')),
-#     path('orders/', include('orders.urls')),
-#     path('realtime/', include('realtime.urls')),  # BuyTogether features
-# ]
-
-# # Dev-only media
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -43,6 +18,5 @@ urlpatterns = [
     path("realtime/", include("realtime.urls")),  # BuyTogether features
 ]
 
-# Dev-only media
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ✅ Serve MEDIA files (product images) in ALL environments (local + Render)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
